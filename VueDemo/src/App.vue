cea91d760f067207756391c4fe16e5e209845215
HJ <2252796369@qq.com>
2019-10-30 12:49:02 +0800

    vue基本案例

---
+++ VueDemo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img class="img" src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+  import HelloWorld from "./components/HelloWorld.vue"
+
+  export default {
+    name: 'App',
+    components:{
+      HelloWorld
+    }
+  }
+</script>
+
+<style scoped>
+.img{
+  width: 800px;
+  height: 400px;
+}
+</style>
---
+++ VueDemo/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="msg">{{msg}}</div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data(){
+      return{
+        msg:"Hello World"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .msg{
+    font-size: 30px;
+  }
+</style>

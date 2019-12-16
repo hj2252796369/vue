208d5a76f4f6a48e62cb47bfe40ef5ff5513219f
hujie <2252796369@qq.com>
2019-12-16 21:37:56 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	VueDemo/.eslintignore
    #	VueDemo/index.html
    #	VueDemo/src-base/components/HelloWorld.vue
    #	VueDemo/src/App.vue

--- VueDemo/src/main.js
+++ VueDemo/src/main.js
@@ -1,11 +1,15 @@
+/*
+入口JS
+ */
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: {
     App
   },
-  template: '<App/>'
+  template: '<App/>',
+  router // 注册路由器
 })
